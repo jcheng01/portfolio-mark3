48d624b5d07337b28f325c4a591e2af4b075cdbb
Justin Cheng <justincheng@Justins-MacBook-Pro.local>
2024-08-27 13:36:23 -0400

    initialize boilerplate

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Justin's Portfolio",
+  description: "Justin is a front end developer...",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+        <div className="bg-[#fbe2e3] -z-10 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.25]"></div>
+        {/* brackets in tailwind for custom HEX colors */}
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.25] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        {children}
+      </body>
+    </html>
+  );
+}
